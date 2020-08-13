a5d08594f5f699c4e84bb3134c0164750a35d974
mcaleb808 <mcaleb808@gmail.com>
2020-08-13 12:42:51 +0200

    create api documentation

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import config from 'dotenv';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../docs/swagger.json';
 
 config.config();
 const app = express();
@@ -7,6 +9,7 @@ const { PORT = 8000 } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (_req, res) => {
   res.status(200).send({
